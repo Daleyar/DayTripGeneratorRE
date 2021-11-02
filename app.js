9c6ebd345d5ae23fcb5c118ea98c828a193c2876
Ali Daley <ali.daley72@gmail.com>
2021-11-02 09:49:37 -0400

    Created a loop for event selection

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let destinations = ['Manhattan','Brooklyn','StatenIsland'];
+let restaurants = ['HibachiBanzai','PizzaZone','CrazyCannolis'];
+let entertainments = ['NightClub','MovieTheater','Circus'];
+let transportations =['Car','Train','Bus']
+
+function generator(arrary){
+    return arrary[Math.floor(Math.random()*arrary.length)];
+}
+
+let counter = 0
+while (counter == 0){
+    let destinationSelected = generator(destinations);
+    let restaurantSelected = generator(restaurants);
+    let entertainmentSelected = generator(entertainments);
+    let transportationSelected = generator(transportations);
+    let resultsSelected = `Destination: ${destinationSelected}\nRestaurant: ${restaurantSelected}\nEntertainment: ${entertainmentSelected}\nTransportation: ${transportationSelected}`;
+if (confirm(`Press OK to confirm your day trip.\nPress Cancel to have destinations re-selected. \n\n${results}`)) {
+    counter = 1;
+    console.log(resultsSelected);
+  }
+  else{
+    continue;
+  }
+}
